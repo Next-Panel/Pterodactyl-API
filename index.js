5e82e3c1dea6cb93eb7778223b8ff5f9bad155ab
Ashu11-A <Matheusn.biolowons@gmail.com>
2023-03-07 11:11:50 -0400

    v2.0.0

--- index.js
+++ index.js
@@ -1,31 +1,50 @@
-// Import all of the files and dependencies needed.
-import fs from 'fs'
-//import express from 'express'
-//import ws from './server/server.js'
-import db from './functions/database.js'
-import setup from './functions/setup.js'
+import fs from 'fs';
+import db from './functions/database.js';
+import setup from './functions/setup.js';
 
-setup(db)
+setup(db);
 
-setTimeout(function main() {
-console.log("Arquivo Criado")
-fs.writeFileSync("status.json", (
-    (`{
-  "daemon": "0.9.999",
-  "panel": "${db.get('panel')}",
-  "wings": "${db.get('wings')}",
-  "sftp": "1.0.5",
-  "discord": "https://discord.gg/Wf8Eycz4Tq",
-  "lastSynced":"${db.get('lastSynced')}"
-}`)
-))}, 13000)
- 
-setTimeout(function main() {
-db.close((err) => {
+function createStatusFile() {
+  console.log("Criando arquivo...");
+  const panel = db.get('panel');
+  const wings = db.get('wings');
+  const lastSynced = db.get('lastSynced');
+
+  // Verificar se algum valor é undefined
+  if (panel === undefined || wings === undefined || lastSynced === undefined) {
+    console.log("Algum valor é indefinido. Tentando novamente em 10 segundos...");
+    setTimeout(createStatusFile, 10000); // Chama a função novamente após 10 segundos
+    return;
+  }
+
+  const data = {
+    "daemon": "0.9.999",
+    panel,
+    wings,
+    "sftp": "1.0.5",
+    "discord": "https://discord.gg/Wf8Eycz4Tq",
+    lastSynced
+  };
+  const json = JSON.stringify(data, null, 2);
+  fs.writeFile("status.json", json, (err) => {
     if (err) {
       console.error(err.message);
     } else {
-      console.log('Banco de dados parado com sucesso');
+      console.log("Arquivo criado com sucesso");
     }
-  })}, 15000
-)
+  });
+
+  // Fechar o banco de dados após 2 segundos
+  setTimeout(function main() {
+    db.close((err) => {
+      if (err) {
+        console.error(err.message);
+      } else {
+        console.log('Banco de dados parado com sucesso');
+      }
+    });
+  }, 2000);
+}
+
+// Chama a função pela primeira vez
+createStatusFile();
